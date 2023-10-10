40c7bd78d474638d7c896cecc7a4e5e7a5bae542
kong0021 <147538496+kong0021@users.noreply.github.com>
2023-10-11 04:22:29 +0800

    added homework 10

---
+++ Homework10/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "homework_10.vg.json";
+vegaEmbed('#barchart', vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
